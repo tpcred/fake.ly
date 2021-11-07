3068e78c443003c599ffcc854e012be78cc74a58
Tiago Cardoso <up201605762@fe.up.pt>
2021-11-07 19:23:42 +0000

    Db knex setup

---
+++ db/knexfile.js
@@ -0,0 +1,19 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'postgres',
+      user:     'postgres',
+      password: 'codes',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    }
+  },
+};
